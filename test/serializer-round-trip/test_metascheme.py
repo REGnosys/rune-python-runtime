1f7ba5c9c4e55b03ac93a734e172af180d5bd2e5
Plamen Neykov <plamen.neykov@cloudrisk.co.uk>
2025-01-26 21:49:45 +0000

    Added full support for enums with metadata

--- test/serializer-round-trip/test_metascheme.py
+++ test/serializer-round-trip/test_metascheme.py
@@ -15,7 +15,6 @@ except ImportError:
 @pytest.mark.skipif(NO_SER_TEST_MOD, reason='Generated test package not found')
 def test_enum_single():
     '''enums with meta'''
-    # import serialization.test.metascheme.Root
     json_str = '''
         {
             "@model": "serialization",
@@ -26,6 +25,31 @@ def test_enum_single():
                 "@scheme": "https://www.example.com/scheme"
             }
         }
+    '''
+    model = BaseDataClass.rune_deserialize(json_str)
+    resp_json = model.rune_serialize()
+    assert json.loads(resp_json) == json.loads(json_str)
+
+
+@pytest.mark.skipif(NO_SER_TEST_MOD, reason='Generated test package not found')
+def test_enum_list():
+    '''list of enums with meta'''
+    json_str = '''
+    {
+        "@model": "serialization",
+        "@type": "serialization.test.metascheme.Root",
+        "@version": "0.0.0",
+        "enumTypeList": [{
+            "@data": "A",
+            "@scheme": "https://www.example.com/scheme1"
+        }, {
+            "@data": "B",
+            "@scheme": "https://www.example.com/scheme2"
+        }, {
+            "@data": "C",
+            "@scheme": "https://www.example.com/scheme3"
+        }]
+    }
     '''
     model = BaseDataClass.rune_deserialize(json_str)
     resp_json = model.rune_serialize()
